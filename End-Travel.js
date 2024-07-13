78a024ccc679335c20879b7d8d28cc5022f1ff78
umaiii <umayarhossainashik48889@gmail.com>
2024-07-13 17:15:54 +0600

    seat colour

---
+++ End-Travel.js
@@ -0,0 +1,428 @@
+
+function   clickOnSeatA1 (){
+    removebgClour   ('A1')
+    setbgcolor      ('A1')
+    removeTxtClour  ('A1')
+    setTxtColor     ('A1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatC1 (){
+    removebgClour   ('C1')
+    setbgcolor      ('C1')
+    removeTxtClour  ('C1')
+    setTxtColor     ('C1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatD1 (){
+    removebgClour   ('D1')
+    setbgcolor      ('D1')
+    removeTxtClour  ('D1')
+    setTxtColor     ('D1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatA2 (){
+    removebgClour   ('A2')
+    setbgcolor      ('A2')
+    removeTxtClour  ('A2')
+    setTxtColor     ('A2')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB2 (){
+    removebgClour   ('B2')
+    setbgcolor      ('B2')
+    removeTxtClour  ('B2')
+    setTxtColor     ('B2')
+    
+}
+
+
+//
+
+
+function   clickOnSeatC2 (){
+    removebgClour   ('C2')
+    setbgcolor      ('C2')
+    removeTxtClour  ('C2')
+    setTxtColor     ('C2')
+    
+}
+
+
+//
+
+
+function   clickOnSeatD2 (){
+    removebgClour   ('D2')
+    setbgcolor      ('D2')
+    removeTxtClour  ('D2')
+    setTxtColor     ('D2')
+    
+}
+
+
+//
+
+
+function   clickOnSeatA3 (){
+    removebgClour   ('A3')
+    setbgcolor      ('A3')
+    removeTxtClour  ('A3')
+    setTxtColor     ('A3')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB3 (){
+    removebgClour   ('B3')
+    setbgcolor      ('B3')
+    removeTxtClour  ('B3')
+    setTxtColor     ('B3')
+    
+}
+
+
+//
+
+
+function   clickOnSeatC3 (){
+    removebgClour   ('C3')
+    setbgcolor      ('C3')
+    removeTxtClour  ('C3')
+    setTxtColor     ('C3')
+    
+}
+
+
+//
+
+
+function   clickOnSeatD3 (){
+    removebgClour   ('D3')
+    setbgcolor      ('D3')
+    removeTxtClour  ('D3')
+    setTxtColor     ('D3')
+    
+}
+
+
+//
+
+
+function   clickOnSeatA4 (){
+    removebgClour   ('A4')
+    setbgcolor      ('A4')
+    removeTxtClour  ('A4')
+    setTxtColor     ('A4')
+    
+}
+
+
+//
+
+
+function   clickOnSeatA4 (){
+    removebgClour   ('A4')
+    setbgcolor      ('A4')
+    removeTxtClour  ('A4')
+    setTxtColor     ('A4')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB4 (){
+    removebgClour   ('B4')
+    setbgcolor      ('B4')
+    removeTxtClour  ('B4')
+    setTxtColor     ('B4')
+    
+}
+
+
+//
+
+
+function   clickOnSeatC4 (){
+    removebgClour   ('C4')
+    setbgcolor      ('C4')
+    removeTxtClour  ('C4')
+    setTxtColor     ('C4')
+    
+}
+
+
+//
+
+
+function   clickOnSeatD4 (){
+    removebgClour   ('D4')
+    setbgcolor      ('D4')
+    removeTxtClour  ('D4')
+    setTxtColor     ('D4')
+    
+}
+
+
+//
+
+
+function   clickOnSeatA5 (){
+    removebgClour   ('A5')
+    setbgcolor      ('A5')
+    removeTxtClour  ('A5')
+    setTxtColor     ('A5')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB5 (){
+    removebgClour   ('B5')
+    setbgcolor      ('B5')
+    removeTxtClour  ('B5')
+    setTxtColor     ('B5')
+    
+}
+
+
+//
+
+
+function   clickOnSeatC5 (){
+    removebgClour   ('C5')
+    setbgcolor      ('C5')
+    removeTxtClour  ('C5')
+    setTxtColor     ('C5')
+    
+}
+
+
+//
+
+
+function   clickOnSeatD5 (){
+    removebgClour   ('D5')
+    setbgcolor      ('D5')
+    removeTxtClour  ('D5')
+    setTxtColor     ('D5')
+    
+}
+
+
+//
+
+
+function   clickOnSeatE5 (){
+    removebgClour   ('E5')
+    setbgcolor      ('E5')
+    removeTxtClour  ('E5')
+    setTxtColor     ('E5')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+function   clickOnSeatB1 (){
+    removebgClour   ('B1')
+    setbgcolor      ('B1')
+    removeTxtClour  ('B1')
+    setTxtColor     ('B1')
+    
+}
+
+
+//
+
+
+
+// BG & TEXT Colour Change 
+function removebgClour (idd){
+    const seat = document.getElementById(idd)
+    seat.classList.remove('bg-[#F7F8F8]');
+}
+function setbgcolor (idds){
+    const seat = document.getElementById(idds)
+    seat.classList.add('greenbg');
+    console.log('zeh')   
+}
+function removeTxtClour (idd){
+    const seat = document.getElementById(idd)
+    seat.classList.remove('Text-Ash');
+}
+function setTxtColor (idds){
+    const seat = document.getElementById(idds)
+    seat.classList.add('text-white');
+    console.log('White')   
+}
+//DONE
+
+function seatLess (cdd){
+    const Count = document.getElementById(cdd)
+    Count.innerText=('5')
+}
+
+
+
+
+
